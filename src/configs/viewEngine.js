7d997e9cb44a13499f0f1bbf9138dfbcff94c3fb
thanh <thanhnp.1402@gmail.com>
2023-03-24 20:03:34 +0700

    video7

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -3,6 +3,6 @@ import expess from "express"
 const configViewEngine = (app) => {
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
-
+    app.use(expess.static('./src/public'))
 }
 export default configViewEngine;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 //const express = require('express');
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+require('dotenv').config();
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
+
+
 
 configViewEngine(app);
+
 app.get('/', (req, res) => {
     res.render('test/index.ejs');
 
